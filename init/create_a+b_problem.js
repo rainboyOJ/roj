cb0dfc1c81a05234f7a019472b763657d35c78b6
rainboy <rainboylvx@qq.com>
2020-09-17 14:43:29 +0800

    fix invalid argument

--- init/create_a+b_problem.js
+++ init/create_a+b_problem.js
@@ -2,10 +2,10 @@
 var ab = {
     "pid":1000,
 		"title": "A + B Problem",
-	  "content":"## 题目描述\n\n输入 $a$ 和 $b$，输出 $a + b$ 的结果。\n\n## 输入格式\n\n一行两个正整数 $a$ 和 $b$。\n\n## 输出格式\n\n一行一个正整数 $a + b$。\n\n## 样例输入\n\n```\n1 2\n```\n\n## 样例输出\n\n```\n3\n```\n## 数据范围与提示\n\n对于 $100\\\\%$ 的数据，$1 \\\\leq a, b \\\\leq 10 ^ 6$。\n",
+	  "content":"## 题目描述\n\n输入 $a$ 和 $b$，输出 $a + b$ 的结果。\n\n## 输入格式\n\n一行两个正整数 $a$ 和 $b$。\n\n## 输出格式\n\n一行一个正整数 $a + b$。\n\n## 样例输入\n\n```\n1 2\n```\n\n## 样例输出\n\n```\n3\n```\n## 数据范围与提示\n\n对于 $100\\%$ 的数据，$1 \\leq a, b \\leq 10 ^ 6$。\n",
 		"time": 1000,
-		"memory": 128*1024*1024,
-		"stack": 128*1024*1024,
+		"memory": 128,
+		"stack": 128,
 		"file_io_input_name": "a+b.in",
 		"file_io_output_name": "a+b.out",
 		"spj": "default",
