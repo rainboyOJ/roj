90b806d0df1627a3c963a92eeb24557121b6ece9
rainboy <rainboylvx@qq.com>
2020-09-19 19:40:40 +0800

    [新功能] isJudgeEnd 被限制3s请求一次

--- Function/sub/isJudgeEnd.js
+++ Function/sub/isJudgeEnd.js
@@ -1,7 +1,30 @@
 // 判断评测结果是不是已经结束
 const DEFINE = require("../../lib/DEFINE")
+const { isMongooseObjectId } = require("../../utils")
+const Cache = require("../../lib/Cache")
 module.exports = async function isJudgeEnd(ctx,next){
+
   let {_id} = ctx.request.body
+
+  if( ! isMongooseObjectId(_id) ){ //非ObjectId格式
+    ctx.redirect("/404")
+    return
+  }
+
+  let KEY = `isJudgeEnd-${_id}`
+  let isJudgeEnd_time_request_limit = await Cache.redis.get(KEY)
+
+  if( isJudgeEnd_time_request_limit ){ //存在
+    ctx.body = {
+      status:0,
+      message:DEFINE.JUDGING,
+      judgeEnd:false
+    }
+    return
+  }
+
+  Cache.redis.set(KEY,1,'EX',3) // 3s才能请求一次
+
   let doc = await db.model['sub'].findOne({_id})
   if( !doc ){
     ctx.redirect("/404")
