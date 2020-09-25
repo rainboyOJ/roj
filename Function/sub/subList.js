97d6fb0b222eec15542b08a67675d157ff76a85a
rainboy <rainboylvx@qq.com>
2020-09-25 23:22:19 +0800

    [add] 提交可以用能 pid,uid过滤了

--- Function/sub/subList.js
+++ Function/sub/subList.js
@@ -8,22 +8,33 @@ const Cache = require("../../lib/Cache")
 
 module.exports = async function subList(ctx,next){
   let page = parseInt(ctx.query.page || 1)
+
   if( isNaN(page)){ //不正确的id
     ctx.redirect("/404")
     return
   }
 
+  let query = {}
+  let {pid,uid} = ctx.query
+  pid = parseInt(pid)
+  if( !isNaN(pid) ) {
+    let problem = await db.GetOneProblem(pid)
+    query.pid   = problem._id
+  }
+  if( uid )         query.uid = uid
+
+
   const pageSize = subList_pageSize; //一页的题目数量
 
-  let docs = await Cache.get(`subList-${page}`,()=>{
-      return db.model['sub'].find().select("-code -result_list").sort({_id:-1}).skip((page-1)*pageSize).limit(pageSize)
+  let docs = await Cache.get(`subList-${page}-${JSON.stringify(query)}`,()=>{
+      return db.model['sub'].find(query).select("-code -result_list").sort({_id:-1}).skip((page-1)*pageSize).limit(pageSize)
     .populate("uid","email username realname")
-    .populate("pid","title pid").then( docs => docs.map(doc => doc.toObject({virtuals: true})) )
+    .populate("pid","title pid").then( docs => docs.map(doc => doc.toJSON({virtuals: true})) )
   })
 
 
-  let count = await Cache.get(`subList-totalsize`,()=>{
-    return db.model['sub'].find().countDocuments()
+  let count = await Cache.get(`subList-totalsize-${JSON.stringify(query)}`,()=>{
+    return db.model['sub'].find(query).countDocuments()
   })
   //debug(docs)
 
